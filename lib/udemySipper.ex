831afea20fe684609314a2b8a90621d0bd207dd7
zhulinpinyu <mulixiang199016@gmail.com>
2017-01-11 22:59:28 +0800

    Init

---
+++ lib/udemySipper.ex
@@ -0,0 +1,18 @@
+defmodule UdemySipper do
+  @moduledoc """
+  Documentation for UdemySipper.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UdemySipper.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/udemySipper_test.exs
@@ -0,0 +1,8 @@
+defmodule UdemySipperTest do
+  use ExUnit.Case
+  doctest UdemySipper
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
