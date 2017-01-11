3873125d2ffa0741f580dec769e33a5a97efbd91
zhulinpinyu <mulixiang199016@gmail.com>
2017-01-12 06:40:11 +0800

    get lecture list

--- lib/udemySipper.ex
+++ lib/udemySipper.ex
@@ -1,18 +1,19 @@
 defmodule UdemySipper do
-  @moduledoc """
-  Documentation for UdemySipper.
-  """
+  def playlist do
+    token = "aSL5hOb3ZCt9AZNa590bFNU3JP1BpwVipsxiVWg9"
+    url = "https://www.udemy.com/api-2.0/courses/1039062/cached-subscriber-curriculum-items?page_size=9999&fields%5Basset%5D=filename,asset_type"
+    headers = ["Authorization": "Bearer #{token}"]
+    {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get(url, headers)
+    %{"results" => results} = Poison.Parser.parse!(body)
+    lectures = results
+                |> Enum.filter(fn(%{"_class" => class}) -> class === "lecture" end )
+                |> Enum.filter(fn(%{"asset" => %{"asset_type" => type}}) -> type === "Video" end)
+                |> Enum.map(&map_lecture/1)
+    IO.inspect lectures
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> UdemySipper.hello
-      :world
-
-  """
-  def hello do
-    :world
+  defp map_lecture(%{"id" => id, "asset" => %{"filename" => filename}}) do
+    filename = Path.basename(filename, "mov") <> "mp4"
+    %{id: id, filename: filename}
   end
 end
--- mix.exs
+++ mix.exs
@@ -28,6 +28,9 @@ defmodule UdemySipper.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:httpoison, "~> 0.10.0"}]
+    [
+      {:httpoison, "~> 0.10.0"},
+      {:poison, "~> 3.0"}
+    ]
   end
 end
