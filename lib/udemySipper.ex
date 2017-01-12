e965c3b389e33274261b9e999eba321795996656
zhulinpinyu <mulixiang199016@gmail.com>
2017-01-12 09:10:21 +0800

    fetch lecture detail

--- lib/udemySipper.ex
+++ lib/udemySipper.ex
@@ -1,19 +1,30 @@
 defmodule UdemySipper do
+  @token "aSL5hOb3ZCt9AZNa590bFNU3JP1BpwVipsxiVWg9"
+  @playlist_url "https://www.udemy.com/api-2.0/courses/1039062/cached-subscriber-curriculum-items?page_size=9999&fields%5Basset%5D=filename,asset_type"
+
   def playlist do
-    token = "aSL5hOb3ZCt9AZNa590bFNU3JP1BpwVipsxiVWg9"
-    url = "https://www.udemy.com/api-2.0/courses/1039062/cached-subscriber-curriculum-items?page_size=9999&fields%5Basset%5D=filename,asset_type"
-    headers = ["Authorization": "Bearer #{token}"]
-    {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get(url, headers)
+    headers = ["Authorization": "Bearer #{@token}"]
+    {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get(@playlist_url, headers)
     %{"results" => results} = Poison.Parser.parse!(body)
     lectures = results
-                |> Enum.filter(fn(%{"_class" => class}) -> class === "lecture" end )
-                |> Enum.filter(fn(%{"asset" => %{"asset_type" => type}}) -> type === "Video" end)
-                |> Enum.map(&map_lecture/1)
-    IO.inspect lectures
+      |> Enum.filter(fn(%{"_class" => class}) -> class === "lecture" end )
+      |> Enum.filter(fn(%{"asset" => %{"asset_type" => type}}) -> type === "Video" end)
+      |> Enum.map(&map_lecture/1)
+      #|> Enum.each(&lecture_detail/1)
+    [h|_t] = lectures
+    lecture_detail(h)
   end
 
   defp map_lecture(%{"id" => id, "asset" => %{"filename" => filename}}) do
     filename = Path.basename(filename, "mov") <> "mp4"
     %{id: id, filename: filename}
+  end
+
+  defp lecture_detail(%{id: id, filename: _filename}) do
+    lecture_url = "https://www.udemy.com/api-2.0/users/me/subscribed-courses/1039062/lectures/#{id}?fields%5Blecture%5D=view_html"
+    headers = ["Authorization": "Bearer #{@token}"]
+    {:ok, %HTTPoison.Response{body: body}} = HTTPoison.get(lecture_url, headers)
+    %{"view_html" => view_html } = Poison.Parser.parse!(body)
+    IO.inspect view_html
   end
 end
