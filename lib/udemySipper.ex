37f168d053ef626b13f181e4e6878207ab159196
zhulinpinyu <mulixiang199016@gmail.com>
2017-06-19 17:13:25 +0800

    添加序号，为每个文件

--- lib/udemySipper.ex
+++ lib/udemySipper.ex
@@ -1,11 +1,8 @@
 defmodule UdemySipper do
-  @token ""
+  @token "BASIC_TOKEN"
 
   def go do
-    courses = [
-      1141696,
-      1075642
-    ]
+    courses = [ 1075642 ]
     courses
     |> Enum.each(&playlist/1)
   end
@@ -13,10 +10,13 @@ defmodule UdemySipper do
   def playlist(course_id) do
     File.mkdir_p!("downloads/courses_#{course_id}")
     playlist_url = "https://www.udemy.com/api-2.0/courses/#{course_id}/cached-subscriber-curriculum-items?page_size=9999&fields%5Basset%5D=filename,asset_type"
+
     %{"results" => results} = fetch_data(playlist_url)
+
     results
       |> Enum.filter(fn(%{"_class" => class}) -> class === "lecture" end )
       |> Enum.filter(fn(%{"asset" => %{"asset_type" => type}}) -> type === "Video" end)
+      |> Enum.with_index()
       |> Enum.map(&map_lecture/1)
       |> Enum.each(fn(lec) -> lecture_detail(lec, course_id) end)
   end
@@ -27,12 +27,14 @@ defmodule UdemySipper do
     Poison.Parser.parse!(body)
   end
 
-  defp map_lecture(%{"id" => id, "asset" => %{"filename" => filename}}) do
-    filename = Path.basename(filename, ".mov") <> ".mp4"
+  defp map_lecture({%{"id" => id, "title" => title}, index}) do
+    #filename = Path.basename(filename, ".mov") <> ".mp4"
+    filename =  "#{(index+1)}.#{title}.mp4"
     %{id: id, filename: filename}
   end
 
   defp lecture_detail(%{id: id, filename: filename},course_id) do
+    IO.puts filename
     lecture_url = "https://www.udemy.com/api-2.0/users/me/subscribed-courses/#{course_id}/lectures/#{id}?fields%5Blecture%5D=view_html"
     %{"view_html" => view_html } = fetch_data(lecture_url)
     [data] = String.replace(view_html, "\\u0026", "&")
@@ -43,7 +45,7 @@ defmodule UdemySipper do
     [%{"src" => src}] = sources
       |> Enum.filter(fn(%{"label" => hd }) -> hd === "720" end)
 
-    download(src,filename,course_id)
+    #download(src,filename,course_id)
   end
 
   defp download(src,filename,course_id) do
